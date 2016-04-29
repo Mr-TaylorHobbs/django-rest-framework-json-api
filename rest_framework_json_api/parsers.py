9e50c1c16c64339f54ac9f68287b227c83e0cd15
TayHobbs <hobbstay@gmail.com>
2016-04-29 15:30:28 -0500

    Feature: Allow bulk creation of objects

--- rest_framework_json_api/parsers.py
+++ rest_framework_json_api/parsers.py
@@ -62,41 +62,48 @@ class JSONParser(parsers.JSONParser):
         data = result.get('data')
 
         if data:
-            from rest_framework_json_api.views import RelationshipView
-            if isinstance(parser_context['view'], RelationshipView):
-                # We skip parsing the object as JSONAPI Resource Identifier Object and not a regular Resource Object
-                if isinstance(data, list):
-                    for resource_identifier_object in data:
-                        if not (resource_identifier_object.get('id') and resource_identifier_object.get('type')):
-                            raise ParseError(
-                                'Received data contains one or more malformed JSONAPI Resource Identifier Object(s)'
-                            )
-                elif not (data.get('id') and data.get('type')):
-                    raise ParseError('Received data is not a valid JSONAPI Resource Identifier Object')
-
-                return data
-
-            request = parser_context.get('request')
-
-            # Check for inconsistencies
-            resource_name = utils.get_resource_name(parser_context)
-            if data.get('type') != resource_name and request.method in ('PUT', 'POST', 'PATCH'):
-                raise exceptions.Conflict(
-                    "The resource object's type ({data_type}) is not the type "
-                    "that constitute the collection represented by the endpoint ({resource_type}).".format(
-                        data_type=data.get('type'),
-                        resource_type=resource_name
-                    )
-                )
-            if not data.get('id') and request.method in ('PATCH', 'PUT'):
-                raise ParseError("The resource identifier object must contain an 'id' member")
-
-            # Construct the return data
-            parsed_data = {'id': data.get('id')}
-            parsed_data.update(self.parse_attributes(data))
-            parsed_data.update(self.parse_relationships(data))
-            parsed_data.update(self.parse_metadata(result))
-            return parsed_data
-
+            if isinstance(data, list):
+                parsed_items = []
+                for d in data:
+                    parsed_items.append(self.parse_data(d, stream, media_type, parser_context, result))
+                return parsed_items
+            return self.parse_data(data, stream, media_type, parser_context, result)
         else:
             raise ParseError('Received document does not contain primary data')
+
+    def parse_data(self, data, stream, media_type, parser_context, result):
+        from rest_framework_json_api.views import RelationshipView
+        if isinstance(parser_context['view'], RelationshipView):
+            # We skip parsing the object as JSONAPI Resource Identifier Object and not a regular Resource Object
+            if isinstance(data, list):
+                for resource_identifier_object in data:
+                    if not (resource_identifier_object.get('id') and resource_identifier_object.get('type')):
+                        raise ParseError(
+                            'Received data contains one or more malformed JSONAPI Resource Identifier Object(s)'
+                        )
+            elif not (data.get('id') and data.get('type')):
+                raise ParseError('Received data is not a valid JSONAPI Resource Identifier Object')
+
+            return data
+
+        request = parser_context.get('request')
+
+        # Check for inconsistencies
+        resource_name = utils.get_resource_name(parser_context)
+        if data.get('type') != resource_name and request.method in ('PUT', 'POST', 'PATCH'):
+            raise exceptions.Conflict(
+                "The resource object's type ({data_type}) is not the type "
+                "that constitute the collection represented by the endpoint ({resource_type}).".format(
+                    data_type=data.get('type'),
+                    resource_type=resource_name
+                )
+            )
+        if not data.get('id') and request.method in ('PATCH', 'PUT'):
+            raise ParseError("The resource identifier object must contain an 'id' member")
+
+        # Construct the return data
+        parsed_data = {'id': data.get('id')}
+        parsed_data.update(self.parse_attributes(data))
+        parsed_data.update(self.parse_relationships(data))
+        parsed_data.update(self.parse_metadata(result))
+        return parsed_data
